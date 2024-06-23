c363a978e0ad4436889e93bdc6a34cec6203b584
Pratyush Kumar <pratyushkr.iiit@gmail.com>
2024-06-23 18:39:17 +0530

    initialized

---
+++ src/portfolio.js
@@ -0,0 +1,66 @@
+const header = {
+  homepage: 'https://yush27.github.io/portfolio/',
+  title: 'Home',
+}
+
+const about = {
+  name: 'Pratyush Kumar',
+  role: 'Computer Science Student at IIIT Kottayam',
+  description:
+    '',
+  resume: 'https://drive.google.com/file/d/1vrDZPYbKZvxfwwkltUY1_39cpS9ykIru/view?usp=sharing',
+  social: {
+    linkedin: 'https://linkedin.com/in/yush27',
+    github: 'https://github.com/yush27',
+  },
+}
+
+const projects = [
+  {
+    name: 'portfolio',
+    description:
+      '',
+    stack: ['React JS'],
+    sourceCode: 'https://github.com/yush27/portfolio',
+    livePreview: 'https://yush27.github.io/portfolio/',
+  },
+  {
+    name: '2048_Game',
+    description:
+      '',
+    stack: ['C++','&', 'Data Structures'],
+    sourceCode: 'https://github.com/yush27/2048_Game',
+    livePreview: 'https://github.com/yush27/2048_Game',
+  },
+  {
+    name: 'Alarm Clock',
+    description:
+      '',
+    stack: ['HTML', 'CSS', 'Java Script'],
+    sourceCode: 'https://github.com/yush27/Alarm-Clock',
+    livePreview: 'https://yush27.github.io/Alarm-Clock/',
+  },
+]
+
+const skills = [
+  'c/c++',
+  'Python',
+  'HTML',
+  'CSS',
+  'JavaScript',
+  'React JS',
+  'Node JS',
+  'Express JS',
+  'Numpy',
+  'Pandas',
+  'Git',
+  'AWS',
+  'MySQL',
+  'MongoDB',
+]
+
+const contact = {
+  email: 'pratyushkr.iiit@mail.com',
+}
+
+export { header, about, projects, skills, contact }
